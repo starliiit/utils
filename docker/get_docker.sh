33aadee8c1839048d1fe09c3d424165f176d62c3
unknown <esu.eru.sl@gmail.com>
2019-09-18 20:54:06 +0800

    feat(get_docker.sh): install docker via USTC

--- docker/get_docker.sh
+++ docker/get_docker.sh
@@ -2,7 +2,11 @@
 
 set -xe
 
-curl -sSL https://get.daocloud.io/docker | sh
+curl -sSL https://get.daocloud.io/docker > /tmp/install_docker.sh
+
+sed -i "s@download.docker.com@mirrors.ustc.edu.cn/docker-ce@g" /tmp/install_docker.sh
+
+source /tmp/install_docker.sh
 
 # docker-compose
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
